1a3179cc2d9ea5bab6157fe81c4c19d7f749ec43
BH02 <956750724@qq.com>
2023-03-27 12:51:18 +0800

    Upload

---
+++ KookBot.py
@@ -0,0 +1,27 @@
+from khl import Bot, Message
+import openai
+
+#KOOK机器人
+bot = Bot(token='KOOK_BOT_TOKEN')
+
+#ChatGPT
+openai.api_key = 'OPENAI_API_KEY'
+GPTmessage = []
+
+@bot.command(regex=r'.*?.*')
+async def chatgpt(msg:Message):
+    GPTmessage.append(
+      {
+        "role": "user",
+        "content": msg.content
+      }
+    )
+    completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=GPTmessage
+     )
+
+    #机器人以回复的形式发送消息
+    await msg.reply(completion.choices[0].message.content)
+
+bot.run()
