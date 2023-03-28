44ef204094a3f5f2e8fff6c4bc1f1fb2eab757f2
BH02 <956750724@qq.com>
2023-03-28 18:04:32 +0800

    本地存储聊天记录
    之后应该加上个只取最后20条消息啥的。

--- KookBot.py
+++ KookBot.py
@@ -1,27 +1,58 @@
 from khl import Bot, Message
 import openai
+import yaml
 
-#KOOK机器人
+# KOOK机器人
 bot = Bot(token='KOOK_BOT_TOKEN')
 
-#ChatGPT
+# ChatGPT
 openai.api_key = 'OPENAI_API_KEY'
-GPTmessage = []
+
+# 读取本地聊天记录
+with open('conversation.yaml', 'r', encoding='utf-8') as getTalk:
+    talk = list(yaml.safe_load_all(getTalk))
+
+    # 存入ChatRecord
+    if len(talk) > 0:
+        ChatRecord = talk[0]
+    else:
+        ChatRecord = []
+
 
 @bot.command(regex=r'[\s\S]*')
-async def chatgpt(msg:Message):
-    GPTmessage.append(
-      {
-        "role": "user",
-        "content": msg.content
-      }
-    )
-    completion = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=GPTmessage
-     )
+async def chatgpt(msg: Message):
+    # 判断消息是否属于指定频道且@了机器人
+    # 频道列表需要自己设置，或者存在config中 ChannelList = ['TEST_CHANNEL','HAVE_FUN']
+    # if msg.extra["channel_name"] in ChannelList and 'BOT_ID' in msg.extra['mention']:
 
-    #机器人以回复的形式发送消息
+    # 去除消息中@机器人的部分
+    UserContent = msg.content.replace('(met)BOT_ID(met) ', '')
+
+    # 往聊天记录添加刚收到的消息
+    ChatRecord.append(
+        {
+            "role": "user",
+            "content": UserContent
+        }
+    )
+
+    # 调用API
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=ChatRecord
+    )
+
+    # 机器人以回复的形式发送消息
     await msg.reply(completion.choices[0].message.content)
+
+    # 写入聊天记录 聊天记录比文件更长则覆盖存入文件 短则追加
+    if len(ChatRecord) > len(talk):
+        with open('conversation.yaml', 'w', encoding='utf-8') as writeTalk:
+            yaml.dump(ChatRecord, writeTalk, allow_unicode=True)
+
+    else:
+        with open('conversation.yaml', 'a', encoding='utf-8') as writeTalk:
+            yaml.dump(ChatRecord, writeTalk, allow_unicode=True)
+
 
 bot.run()
