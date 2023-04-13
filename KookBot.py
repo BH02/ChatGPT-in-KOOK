a4471aacef780a628a5ec07cd7392b8ce4b00b0f
BH02 <956750724@qq.com>
2023-04-13 16:16:08 +0800

    Update KookBot.py
    修改参数的判断方式

--- KookBot.py
+++ KookBot.py
@@ -11,50 +11,50 @@ openai.api_key = 'OPENAI_API_KEY'
 
 
 # /gpt 发送聊天内容 /gpt own 则是专属于单个用户的聊天
-@bot.command(regex=r'[\s\S]*')
-async def gpt(msg: Message):
-    if '/gpt' in msg.content:
-
-        # 检查是否是用户专属的聊天
-        specified = True if 'own' in msg.content else False
-
-        # 拿到发消息的用户id，不是专属聊天就拿频道id
-        ChatID = msg.extra['author']['id'] if specified else msg.target_id
-        # 指定聊天记录文件的路径
-        ChatFile = f'ChatRecord/chat-{ChatID}.yaml'
-
-        # 判断是否存在旧的聊天记录，有则读取，无则新建
-        if os.path.exists(ChatFile):
-            with open(ChatFile, 'r', encoding='utf-8') as getTalk:
-                data = yaml.safe_load(getTalk)
-
-                # 新建的文件获取到的getTal会为空，需要处理一下
-                talk = data if data else []
+@bot.command()
+async def gpt(msg: Message, *command):
+    # 检查是否使用户专属聊天
+    if 'own' in command:
+        # 专属聊天拿用户的ID作为文件名
+        ChatFile = 'ChatRecord/chat-' + msg.extra['author']['id'] + '.yaml'
+        chat = command[1]
+    else:
+        if command:
+            # 不是专属聊天拿频道ID作为文件名
+            ChatFile = 'ChatRecord/chat-' + msg.target_id + '.yaml'
+            chat = command[0]
         else:
-            # 新建文件
-            os.mknod(ChatFile)
-            talk = []
+            await msg.reply('消息内容为空')
+            return
 
-        # 去除指令内容，更美观
-        chat = msg.content.replace('/gpt ', '').replace('own', '')
-        LatestChat = {"role": "user", "content": chat}
+    # 判断是否存在旧的聊天记录，有则读取，无则新建
+    if os.path.exists(ChatFile):
+        with open(ChatFile, 'r', encoding='utf-8') as getTalk:
+            data = yaml.safe_load(getTalk)
+            # 新建的文件获取到的getTal会为空，需要处理一下
+            talk = data if data else []
+    else:
+        # 新建文件
+        os.mknod(ChatFile)
+        talk = []
 
-        # 存入ChatRecord
-        ChatRecord = talk if len(talk) < 20 else talk[-20:]
+    LatestChat = {"role": "user", "content": chat}
 
-        # 往聊天记录添加刚收到的消息
-        ChatRecord.append(LatestChat)
+    # 存入ChatRecord
+    ChatRecord = talk if len(talk) < 20 else talk[-20:]
 
-        # 调用API
-        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                  messages=ChatRecord)
+    # 往聊天记录添加刚收到的消息
+    ChatRecord.append(LatestChat)
 
-        # 回复信息
-        await msg.reply(completion.choices[0].message.content)
+    # 调用API
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                              messages=ChatRecord)
+    # 回复信息
+    await msg.reply(completion.choices[0].message.content)
 
-        # 往聊天记录文件里追加最新的消息
-        with open(ChatFile, 'a', encoding='utf-8') as writeTalk:
-            yaml.dump([LatestChat], writeTalk, allow_unicode=True, default_flow_style=False)
+    # 往聊天记录文件里追加最新的消息
+    with open(ChatFile, 'a', encoding='utf-8') as writeTalk:
+        yaml.dump([LatestChat], writeTalk, allow_unicode=True, default_flow_style=False)
 
 
 bot.run()
